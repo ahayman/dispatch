40198fca842a4f92d787d0f557bb85a85311e328
Aaron Hayman <aaron@flexile.co>
2017-03-07 09:01:42 -0600

    - Created Dispatch, copied over from my RxStream project

---
+++ Dispatch/Dispatch/Dispatch.h
@@ -0,0 +1,19 @@
+//
+//  Dispatch.h
+//  Dispatch
+//
+//  Created by Aaron Hayman on 3/7/17.
+//  Copyright © 2017 Aaron Hayman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dispatch.
+FOUNDATION_EXPORT double DispatchVersionNumber;
+
+//! Project version string for Dispatch.
+FOUNDATION_EXPORT const unsigned char DispatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dispatch/PublicHeader.h>
+
+
