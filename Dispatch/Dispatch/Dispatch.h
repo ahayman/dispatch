24e332dd2a68f99f0d7c9b661d1d193a5b8c3e85
Aaron Hayman <aaron@flexile.co>
2017-03-07 09:09:35 -0600

    - Added unit tests - Renamed the target, as it was conflicting with Apple’s

--- Dispatch/Dispatch/Dispatch.h
+++ Dispatch/Dispatch/Dispatch.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Aaron Hayman. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Dispatch.
 FOUNDATION_EXPORT double DispatchVersionNumber;
--- Dispatch/DispatchTests/DispatchTests.swift
+++ Dispatch/DispatchTests/DispatchTests.swift
@@ -7,30 +7,145 @@
 //
 
 import XCTest
-@testable import Dispatch
+import Dispatcher
+
+private class WeakBox<T: AnyObject> {
+  weak var value: T?
+  init(value: T) { self.value = value }
+}
 
 class DispatchTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  func testDelayedDispatch() {
+    let now = NSDate.timeIntervalSinceReferenceDate
+    var done = NSDate.timeIntervalSinceReferenceDate
+    let expectUpdate = expectation(description: "expect the date to be updated")
+    Dispatch.after(delay: 2.0, on: .main).execute {
+      done = NSDate.timeIntervalSinceReferenceDate
+      expectUpdate.fulfill()
     }
+    waitForExpectations(timeout: 3.0)
+    XCTAssert(done - now >= 2.0)
+  }
+  
+  func testDispatchMain() {
+    let expectMain = expectation(description: "dispatched on main.")
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    DispatchQueue.global().async {
+      Dispatch.async(on: .main).execute {
+        if Thread.isMainThread {
+          expectMain.fulfill()
         }
+      }
     }
     
+    waitForExpectations(timeout: 5.0)
+  }
+  
+  func testDispatchBackground() {
+    let expectMain = expectation(description: "dispatched in background.")
+    
+    Dispatch.async(on: .background).execute {
+      if !Thread.isMainThread {
+        expectMain.fulfill()
+      }
+    }
+    
+    waitForExpectations(timeout: 5.0)
+  }
+  
+  func testChaining() {
+    var dispatches = [String]()
+    let expectChain = expectation(description: "chain execution complete")
+    
+    Dispatch
+      .async(on: .background).execute {
+        guard !Thread.isMainThread else { return }
+        dispatches.append("background1")
+      }.then(.async(on: .main)) {
+        guard Thread.isMainThread else { return }
+        dispatches.append("main1")
+      }.then(.async(on: .background)) {
+        guard !Thread.isMainThread else { return }
+        dispatches.append("background2")
+        expectChain.fulfill()
+    }
+    
+    waitForExpectations(timeout: 5.0)
+    guard dispatches.count == 3 else { return XCTFail("The dispatch chain did not complete. Expected 3 dispatches, received \(dispatches.count)") }
+    XCTAssertEqual(dispatches[0], "background1")
+    XCTAssertEqual(dispatches[1], "main1")
+    XCTAssertEqual(dispatches[2], "background2")
+  }
+  
+  func testImmediateChaining() {
+    var dispatches = [String]()
+    let expectChain = expectation(description: "chain execution complete")
+    
+    // Using `sync` will make sure work is executed prior to additional work being added.  We just want to make sure the chain isn't broken.
+    Dispatch
+      .sync(on: .main).execute {
+        guard Thread.isMainThread else { return }
+        dispatches.append("main1")
+      }.then(.sync(on: .main)) {
+        guard Thread.isMainThread else { return }
+        dispatches.append("main2")
+      }.then(.sync(on: .main)) {
+        guard Thread.isMainThread else { return }
+        dispatches.append("main3")
+        expectChain.fulfill()
+    }
+    
+    waitForExpectations(timeout: 5.0)
+    guard dispatches.count == 3 else { return XCTFail("The dispatch chain did not complete. Expected 3 dispatches, received \(dispatches.count)") }
+    XCTAssertEqual(dispatches[0], "main1")
+    XCTAssertEqual(dispatches[1], "main2")
+    XCTAssertEqual(dispatches[2], "main3")
+  }
+  
+  func testSerialReentryDetection() {
+    let dispatch = Dispatch.sync(on: .custom(CustomQueue(type: .serial, name: "TestSerialQueue")))
+    let outerDispatch = expectation(description: "Outer dispatch executed.")
+    let innerDispatch = expectation(description: "Inner dispatch executed without locking.")
+    dispatch.execute {
+      outerDispatch.fulfill()
+      // Re-entry
+      dispatch.execute {
+        innerDispatch.fulfill()
+      }
+    }
+    waitForExpectations(timeout: 5.0)
+  }
+  
+  func testReferenceCycleBreak() {
+    var chains = [WeakBox<ExecutionChain>]()
+    let expectChainCompletion = expectation(description: "Chain has completed")
+    let expectTimeout = expectation(description: "Deinit Timeout")
+    
+    var chain = Dispatch.async(on: .main).execute { }
+    chains.append(WeakBox(value: chain))
+    
+    chain = chain.then(.async(on: .background)) {  }
+    chains.append(WeakBox(value: chain))
+    
+    chain = chain.then(.async(on: .main)) {  }
+    chains.append(WeakBox(value: chain))
+    
+    chain = chain.then(.async(on: .background)) {
+      expectChainCompletion.fulfill()
+    }
+    chains.append(WeakBox(value: chain))
+    
+    chain = Dispatch.after(delay: 1.0, on: .main).execute {
+      expectTimeout.fulfill()
+    }
+    
+    waitForExpectations(timeout: 3.0)
+    
+    XCTAssertNil(chains[0].value)
+    XCTAssertNil(chains[1].value)
+    XCTAssertNil(chains[2].value)
+    XCTAssertNil(chains[3].value)
+  }
+  
 }
